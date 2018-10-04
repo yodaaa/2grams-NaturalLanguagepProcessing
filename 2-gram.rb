9ef2f5eca52ae4aae7cf1a3bd7993786cd46f0dc
yodaaa <yodaaa@MacBookProyodaaa.local>
2018-10-04 16:41:12 +0900

    表作成まで

--- 2-gram.rb
+++ 2-gram.rb
@@ -6,9 +6,36 @@ class String
   end
 end
 
+
+
 f = File.open("doc1.txt")
 str = f.read
-2gramstr = str.to_ngram(2)
-puts 22gramstr
-
+gramstr = str.to_ngram(2)
+#puts 2gramstr
+#puts "t".ord
 f.close
+
+table = [*'aa'..'zz']
+#puts table[675] # table[0] => aa  table[26] => ba fin[675]
+num = 0
+gramtable = Array.new(676)
+gramtable.map!(&:to_i)
+
+table.each do |i|
+  gramstr.each do |j|
+    if i == j
+      gramtable[num] = gramtable[num] + 1
+    end
+  end
+  num = num +1
+end
+
+num =1
+gramtable.each do |i|
+  print sprintf("%03d, ",i)
+
+  if num % 26 == 0
+    puts
+  end
+  num = num +1
+end
