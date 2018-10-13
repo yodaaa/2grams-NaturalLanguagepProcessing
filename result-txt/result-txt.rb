8023eae49e0c11e3bc2fe61ba59e10c516907cc6
yodaaa <01ewell0@gmail.com>
2018-10-14 00:02:39 +0900

    each文 修正

--- result-txt/result-txt.rb
+++ result-txt/result-txt.rb
@@ -9,8 +9,7 @@ File.open("result.txt", mode = "rt"){|f|
   f.each_line{|line|
     #p line
 
-    count = 0
-    ["動詞","形容詞","形容動詞","名詞","副詞","連体詞","接続詞","感動詞","助詞","助動詞","記号"].each { |i|
+    ["動詞","形容詞","形容動詞","名詞","副詞","連体詞","接続詞","感動詞","助詞","助動詞","記号"].each_with_index { |i, count|
       if line.slice(i) != nil
         p line.slice(i)
         p count
@@ -19,8 +18,6 @@ File.open("result.txt", mode = "rt"){|f|
       end
       count = count +1
     }
-
-
   }
-      puts "品詞頻度: #{cnt}"
+  puts "品詞頻度: #{cnt}"
 }
